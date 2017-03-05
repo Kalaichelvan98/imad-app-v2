d8cb31156a10a3e357f9887fb3bb9970c5073264
Kalaichelvan98 <kalaichelvan.kc@gmail.com>
2017-03-05 14:50:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,11 @@
-console.log('Loaded!');
-var img=document.getElementbyId('modi');
-var marginLeft=0;
-function moveright(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRigth,50);
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    
+    
+    
+    
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
