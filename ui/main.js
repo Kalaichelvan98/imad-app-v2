0d75c958d8c9f5236488686b7b0d7ef19b8ab6c3
Kalaichelvan98 <kalaichelvan.kc@gmail.com>
2017-03-05 14:39:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img=document.getElementbyId('modi');
+var marginLeft=0;
+function moveright(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRigth,50);
+};
